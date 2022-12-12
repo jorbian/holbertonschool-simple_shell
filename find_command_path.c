340004a02cd3aaf737ad3ebde349c85981dff1e6
jorbian <5533@holbertonstudents.com>
2022-12-12 00:08:37 -0600

    Memory leaks finally fixed.

--- find_command_path.c
+++ find_command_path.c
@@ -9,11 +9,13 @@
 char *find_command_path(SimpleShell_t *shell)
 {
 	struct stat file_info;
-	char *test_path;
 	char *command_path;
+	char *test_path;
 	int i = 0;
 
 	command_path = malloc(sizeof(char) * 255);
+	
+	snprintf(command_path, 255, "%s", shell->command_args[0]);
 
 	while (shell->path_variable[i])
 	{
@@ -28,7 +30,6 @@ char *find_command_path(SimpleShell_t *shell)
 		free(test_path);
 	}
 	free(command_path);
-	free(test_path);
 
 	return (NULL);
 }
